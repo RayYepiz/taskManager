c549bfcf1882bdd7af252b72b55182cbb05e6008
Raymond Yepiz <93514024+RayYepiz@users.noreply.github.com>
2024-10-02 17:53:51 -0700

    Added Form logic and Task component. Working on finishing Form submission.

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,4 +1,5 @@
-import { react, useState, useRef } from "react";
+import { react, useState } from "react";
+import Task from "./task";
 
 function MyForm() {
   const [formData, setFormData] = useState({
@@ -9,63 +10,108 @@ function MyForm() {
     description: "",
   });
 
-  let handleChange = (key, value) => {
-    setFormData((preFormData)=>({
-        ...preFormData,
-        [key]: value,
-      })
-      
-    );
+  const [tasks, setTasks] = useState([]);
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const clear = () => {
+    setFormData({
+      title: "",
+      dueDate: "",
+      priority: "",
+      status: "",
+      description: "",
+    });
   };
 
-  function submitTask() {
-    console.log("form submitted");
+  let handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((preFormData) => ({
+      ...preFormData,
+      [id]: value,
+    }));
+  };
+
+  function submitTask(e) {
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    if (formData) {
+      const newTask = {
+        title: formData.title,
+        dueDate: formData.dueDate,
+        priority: formData.priority,
+        status: formData.status,
+        description: formData.description,
+      };
+
+      setTasks((prevTasks) => [...prevTasks, newTask]);
+      clear();
+    } else {
+      console.error("Form data is undefined");
+    }
   }
 
   const clearForm = (e) => {
-    console.log("cleared");
+    e.preventDefault();
+    setIsSubmitted(false);
+    clear();
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitTask}>
         <label htmlFor="taskTitle"> Title </label>
         <input
           type="text"
-          id="taskTitle"
+          id="title"
+          value={formData.title}
           onChange={handleChange}
           placeholder="Title of your Task"
         />
 
         <label htmlFor="taskDueDate"> Due Date </label>
-        <input type="date" id="taskDueDate" />
+        <input
+          type="date"
+          id="dueDate"
+          value={formData.dueDate}
+          onChange={handleChange}
+        />
 
-        <label htmlFor="taskPriority"> Priority </label>
-        <select id="taskPriority" name="taskPriority">
+        <label htmlFor="priority"> Priority </label>
+        <select id="priority" value={formData.priority} onChange={handleChange}>
           <option value="">Select a Priority</option>
           <option value="High">High</option>
           <option value="Medium">Medium</option>
           <option value="Low">Low</option>
         </select>
 
-        <label htmlFor="taskStatus"> Status </label>
-        <select id="taskStatus" name="taskStatus">
+        <label htmlFor="status"> Status </label>
+        <select id="status" value={formData.status} onChange={handleChange}>
           <option value="">Select a Status</option>
-          <option value="High">Completed</option>
-          <option value="Medium">Working</option>
-          <option value="Low">Not Started</option>
+          <option value="completed">Completed</option>
+          <option value="working">Working</option>
+          <option value="notStarted">Not Started</option>
         </select>
 
-        <label htmlFor="taskDescription"> Description </label>
-        <textarea name="taskDescription" id="taskDescription"></textarea>
+        <label htmlFor="description"> Description </label>
+        <textarea
+          id="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
 
-        <button type="submit" id="formSubmitBtn" onClick={submitTask}>
+        <button type="submit" id="formSubmitBtn">
           Submit
         </button>
-        <button className="formClearBtn" onClick={clearForm}>
+        <button id="formClearBtn" onClick={clearForm}>
           Clear
         </button>
       </form>
+      {tasks.map((task) => {
+        <Task task={task} />;
+      })}
+      <h1>{JSON.stringify(tasks)}</h1>
     </div>
   );
 }
---
+++ src/components/Task.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Task({ tasks }) {
+  return (
+    <>
+      {tasks.map((t, index) => {
+        if (t) {
+          <div className="taskWrapper" key={index}>
+            <div className="front">
+              <h1>Title: {t.title}</h1>
+            </div>
+            <div className="back">
+              <h2>Due Date: {t.dueDate}</h2>
+              <h2>Priority: {t.priority}</h2>
+              <h2>Status: {t.status}</h2>
+              <h2>Description: {t.description}</h2>
+            </div>
+          </div>;
+        }
+      })}
+    </>
+  );
+}
+
+export default Task;
